5da7d53b6c8040600ac638557d5d76068eed40d7
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-03-04 08:45:59 -0300

    refactor tasks

--- script.js
+++ script.js
@@ -1,82 +1,51 @@
-const values = [ "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", ];
-const suits = ["♥", "♠", "♦", "♣"];
-const cards = [];
+const cards = [
+  {value:'A', suit: '♥'}, {value:'2', suit: '♥'}, {value:'3', suit: '♥'}, {value:'4', suit: '♥'}, {value:'5', suit: '♥'}, {value:'6', suit: '♥'}, {value:'7', suit: '♥'}, {value:'8', suit: '♥'},  {value:'9', suit: '♥'}, {value:'10', suit: '♥'}, {value:'J', suit: '♥'}, {value:'Q', suit: '♥'}, {value:'K', suit: '♥'},
+  {value:'A', suit: '♠'}, {value:'2', suit: '♠'}, {value:'3', suit: '♠'}, {value:'4', suit: '♠'}, {value:'5', suit: '♠'}, {value:'6', suit: '♠'}, {value:'7', suit: '♠'}, {value:'8', suit: '♠'},  {value:'9', suit: '♠'}, {value:'10', suit: '♠'}, {value:'J', suit: '♠'}, {value:'Q', suit: '♠'}, {value:'K', suit: '♠'},
+  {value:'A', suit: '♦'}, {value:'2', suit: '♦'}, {value:'3', suit: '♦'}, {value:'4', suit: '♦'}, {value:'5', suit: '♦'}, {value:'6', suit: '♦'}, {value:'7', suit: '♦'}, {value:'8', suit: '♦'},  {value:'9', suit: '♦'}, {value:'10', suit: '♦'}, {value:'J', suit: '♦'}, {value:'Q', suit: '♦'}, {value:'K', suit: '♦'},
+  {value:'A', suit: '♣'}, {value:'2', suit: '♣'}, {value:'3', suit: '♣'}, {value:'4', suit: '♣'}, {value:'5', suit: '♣'}, {value:'6', suit: '♣'}, {value:'7', suit: '♣'}, {value:'8', suit: '♣'},  {value:'9', suit: '♣'}, {value:'10', suit: '♣'}, {value:'J', suit: '♣'}, {value:'Q', suit: '♣'}, {value:'K', suit: '♣'}
+];
+let filter;
 const main = document.getElementsByClassName('cards')[0]
 
-function deck() {
-    for (let s = 0; s < suits.length; s++) {
-      for (let v = 0; v < values.length; v++) {
-        let card = {
-        value: values[v],
-        suit:suits[s]
-        }
-        cards.push(card);
-      }
-    }
-    return cards;
-
-}
-console.log(deck());
-
-createDeckOfCards = cards => {
+function createDeckOfCards(cards){
   return cards.map(card => {
     const el = document.createElement('div');
+    el.className = 'card'
     el.innerHTML = card.value + card.suit;
-    return el
+      if (card.suit === '♥' || card.suit === '♦'){
+        el.classList.add('card--red')
+      }
+    return el;
   });
 }
-
-const deckOfCards = createDeckOfCards(cards);
-
-deckOfCards.forEach(cards => {
-  main.appendChild(cards);
-  cards.className = 'card';
-})
+function displayCards() {
+  let cardsList = filter ? cards.filter(card => card.suit === filter) : cards;
+  const filteredDeck = createDeckOfCards(cardsList);
+  main.replaceChildren(...filteredDeck);
+}
 
 let heartButton = document.querySelector('#hearts')
 heartButton.addEventListener('click', () => {
-  const heartCards = cards.filter(card => card.suit === '♥')
-
-  const heartDeck = createDeckOfCards(heartCards);
-
-  heartDeck.forEach(cards => {
-    main.replaceChildren(...heartDeck)
-    cards.className = 'card';
-  })
+  filter = '♥';
+  displayCards();
 })
 
 let spadeButton = document.querySelector('#spades')
 spadeButton.addEventListener('click', () => {
-  const spadeCards = cards.filter(card => card.suit === '♠')
-
-  const spadeDeck = createDeckOfCards(spadeCards);
-
-  spadeDeck.forEach(cards => {
-    main.replaceChildren(...spadeDeck)
-    cards.className = 'card';
-  })
+  filter = '♠';
+  displayCards();
 })
 
 let diamondButton = document.querySelector('#diamonds')
 diamondButton.addEventListener('click', () => {
-  const diamondCards = cards.filter(card => card.suit === '♦')
-
-  const diamondDeck = createDeckOfCards(diamondCards);
-
-  diamondDeck.forEach(cards => {
-    main.replaceChildren(...diamondDeck)
-    cards.className = 'card';
-  })
+  filter = '♦';
+  displayCards();
 })
 
 let clubButton = document.querySelector('#clubs')
 clubButton.addEventListener('click', () => {
-  const clubCards = cards.filter(card => card.suit === '♣')
-
-  const clubDeck = createDeckOfCards(clubCards);
-
-  clubDeck.forEach(cards => {
-    main.replaceChildren(...clubDeck)
-    cards.className = 'card';
-  })
+  filter = '♣';
+  displayCards();
 })
+
+displayCards();
